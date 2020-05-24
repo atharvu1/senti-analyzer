bdc057f566bd2718e10ec54735db6e20cfe1f38d
atharu1 <atharvu1@gmail.com>
2020-05-24 19:54:15 +0530

    Updates

---
+++ precache-manifest.792e683f7a92fe3ef4aa28e87036c40d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53438666276b8dc652a4301c3d55bb7d",
+    "url": "/Senti-Analyzer/index.html"
+  },
+  {
+    "revision": "50a323537e9707d8f922",
+    "url": "/Senti-Analyzer/static/css/main.aa41cee9.chunk.css"
+  },
+  {
+    "revision": "de253a14db12f3393b40",
+    "url": "/Senti-Analyzer/static/js/2.99ad8c47.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Senti-Analyzer/static/js/2.99ad8c47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50a323537e9707d8f922",
+    "url": "/Senti-Analyzer/static/js/main.4ef4a6d0.chunk.js"
+  },
+  {
+    "revision": "fd5b8d0ed97df8e57ce7",
+    "url": "/Senti-Analyzer/static/js/runtime-main.3f8ccc4e.js"
+  },
+  {
+    "revision": "0b94554231f7623f1de956a6cdca2bee",
+    "url": "/Senti-Analyzer/static/media/bkg.0b945542.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Senti-Analyzer/static/media/logo.5d5d9eef.svg"
+  }
+]);
